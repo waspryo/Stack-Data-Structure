8425ee1414618c9c5c58c2afb77d43613786561b
waspryo <sugimoto@liddell.tokyo>
2020-06-06 15:15:37 +0900

    pop

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ class Stack {
     this.count = 0
   }
   // Add element to top of stack
+
+  // return and remove top element in stack
+  // return undefind if stack is empty
+  pop() {
+    if (this.count == 0) {return undefined}
+    let deleteItem = this.items[this.count -1]
+    this.count -= 1
+    console.log(`${deleteItem} removed`);
+    return deleteItem
+  }
 }
 
 const stack = new Stack()
@@ -12,3 +22,5 @@ stack.push(100)
 stack.push(200)
 stack.push(300)
 stack.push(400)
+
+stack.pop()
